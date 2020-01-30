edd2e7b374a39a53724e3d47e8a36409d52b447d
syedfaizalalatas <syedfaizalalatas@gmail.com>
2020-01-30 12:00:39 +0800

    commit new file duplicated as example

---
+++ application/views/user_view2.php
@@ -0,0 +1,37 @@
+<?php
+ //echo "i am a view for user details.";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>user details</title>
+</head>
+<body>
+ <table border="2">
+  <tr>
+   <th>User Id</th>
+   <th>User Name</th>
+   <th>Company Name</th>
+  </tr>
+  <?php 
+   foreach ($userArray2 as $key => $value) {
+    ?>
+    <tr>
+     <td>&nbsp;<?= $value->id; ?></td>
+     <td>&nbsp;<?= $value->username; ?></td>
+     <td>&nbsp;<?= $value->company; ?></td>
+    </tr>
+    <?php
+   }
+  ?>
+ </table>
+
+ </body>
+</html>
+
+<?php
+ //echo "<pre>";
+ //print_r($userArray);
+ //echo "</pre>";
+?>
